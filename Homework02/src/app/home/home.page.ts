5fb4e6192cb07b707c0d551120d1eec537a1c810
unknown <njw32@students.calvin.edu>
2019-10-17 20:36:02 -0400

    storage in app, module

--- Homework02/src/app/home/home.page.ts
+++ Homework02/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NativeStorage} from '@ionic-native/native-storage/ngx';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,6 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
-
+  private nativeStorage: NativeStorage
 }
+
