d4b97157d05a9fe0eca352b8fc9ad69525dd868e
unknown <njw32@students.calvin.edu>
2019-10-17 21:22:06 -0400

    methods

--- Homework02/src/app/home/home.page.ts
+++ Homework02/src/app/home/home.page.ts
@@ -8,7 +8,17 @@ import { NativeStorage} from '@ionic-native/native-storage/ngx';
 })
 export class HomePage {
 
-  constructor() {}
-  private nativeStorage: NativeStorage
+  constructor(private nativeStorage: NativeStorage) {
+    this.nativeStorage.setItem('test', {property: 'value', anotherProperty: 'anotherValue'})
+        .then(
+          (data) => console.log('Stored first item!', data),
+          error => console.error('Error storing item', error))
+    this.nativeStorage.getItem('test')
+          .then(
+            data => console.log(data),
+            error => console.error(error)
+          );
 }
+}
+
 
