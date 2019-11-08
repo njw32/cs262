b17a5e0bd673b60773516a9b26d95bd653067882
unknown <njw32@students.calvin.edu>
2019-11-08 18:50:06 -0500

    ionic gen

--- Homework03/app/ionic-gallery/src/app/app-routing.module.ts
+++ Homework03/app/ionic-gallery/src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'manage-gallery-item', loadChildren: './manage-gallery-item/manage-gallery-item.module#ManageGalleryItemPageModule' },
+  { path: 'view-gallery', loadChildren: './view-gallery/view-gallery.module#ViewGalleryPageModule' },
 ];
 
 @NgModule({
---
+++ Homework03/app/ionic-gallery/src/app/view-gallery/view-gallery.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-gallery',
+  templateUrl: './view-gallery.page.html',
+  styleUrls: ['./view-gallery.page.scss'],
+})
+export class ViewGalleryPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
