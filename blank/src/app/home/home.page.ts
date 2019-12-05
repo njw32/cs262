0ef9a0fdc5e42102c11414b3387719b1bb86f905
unknown <njw32@students.calvin.edu>
2019-12-05 18:22:38 -0500

    table creation

--- blank/src/app/home/home.page.ts
+++ blank/src/app/home/home.page.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
-
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+    private database: SQLiteObject;
+  
+    constructor(public sqlite: SQLite){
+      console.log('Constructor')
+      this.sqlite.create(
+        {
+        name: 'ex.db',
+        location: 'default' 
+        
+      }).then(
+          (db : SQLiteObject) => {
+            console.log('database created')
+            // console.log(JSON.stringify(db))
+            db.executeSql('CREATE TABLE table (name varchar)')
+            console.log('table created')
 
-  constructor() {}
-
+          }
+            )
+          }
+          
+        // addHero(dbObj: SQLiteObject){
+        //   let q = "INSERT INTO heros (name) VALUES(?)"
+        //   return dbObj.executeSql(q, ['Iron Man']).then(
+        //     (res) => {
+        //       return res
+        //   }
+        //   )
+        // }
+        // printAllHeros(dbObj: SQLiteObject){
+        //   let q = "SELECT name FROM heros"
+        //   dbObj.executeSql(q,[]).then (
+        //     (res) =>{
+        //       for(let i =0; i< res.rows.length; i++){
+        //         console.log(res.rows.item(i))
+        //       }
+        //     }
+        //   )
+      
+        // }
 }
