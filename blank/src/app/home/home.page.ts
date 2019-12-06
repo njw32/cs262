f361a28faa10ce634d0c90fe6996556df87e2800
unknown <njw32@students.calvin.edu>
2019-12-05 19:13:03 -0500

    uncomment

--- blank/src/app/home/home.page.ts
+++ blank/src/app/home/home.page.ts
@@ -15,34 +15,40 @@ export class HomePage {
         name: 'ex.db',
         location: 'default' 
         
-      }).then(
+       })
+       .then(
           (db : SQLiteObject) => {
             console.log('database created')
             // console.log(JSON.stringify(db))
-            db.executeSql('CREATE TABLE table (name varchar)')
-            console.log('table created')
-
-          }
+            this.addHero(db).then(
+              (res) => {
+                console.log('hero added')
+                console.log(res)
+                this.printAllHeros(db)
+              }
             )
           }
+        )
+        
+             }
           
-        // addHero(dbObj: SQLiteObject){
-        //   let q = "INSERT INTO heros (name) VALUES(?)"
-        //   return dbObj.executeSql(q, ['Iron Man']).then(
-        //     (res) => {
-        //       return res
-        //   }
-        //   )
-        // }
-        // printAllHeros(dbObj: SQLiteObject){
-        //   let q = "SELECT name FROM heros"
-        //   dbObj.executeSql(q,[]).then (
-        //     (res) =>{
-        //       for(let i =0; i< res.rows.length; i++){
-        //         console.log(res.rows.item(i))
-        //       }
-        //     }
-        //   )
+        addHero(dbObj: SQLiteObject){
+          let q = "INSERT INTO heros (name) VALUES(?)"
+          return dbObj.executeSql(q, ['Iron Man']).then(
+            (res) => {
+              return res
+          }
+          )
+        }
+        printAllHeros(dbObj: SQLiteObject){
+          let q = "SELECT name FROM heros"
+          dbObj.executeSql(q,[]).then (
+            (res) =>{
+              for(let i =0; i< res.rows.length; i++){
+                console.log(res.rows.item(i))
+              }
+            }
+          )
       
-        // }
+        }
 }
